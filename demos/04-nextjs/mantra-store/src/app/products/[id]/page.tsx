75716be90210c2f58261184d7c41dffd72d98c13
Prashanth Puranik <puranik@digdeeper.in>
2025-04-07 10:41:02 +0530

    Step 19: Discussion on SSG / SSR / ISG

--- demos/04-nextjs/mantra-store/src/app/products/[id]/page.tsx
+++ demos/04-nextjs/mantra-store/src/app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { getProductById, getProductIds } from "@/data/services/products";
 import ProductDetail from "@/components/product-detail/product-detail";
 import type { IProduct } from "@/types/Product";
 import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 type Props = {
     params: { id: string };
@@ -35,10 +36,15 @@ export async function generateStaticParams() {
 }
 
 // in case of ISR (revalidate is exported), it runs periodically
+// in case product is not one of them generated at build-time, this function runs at request time! In case such a product exists, the page is shown, else not found page is shown
 export default async function ProductDetailPage({ params }: Props) {
     const { id } = await params;
 
     const product: IProduct = await getProductById(id);
+
+    if (!product) {
+        return notFound();
+    }
 
     return <ProductDetail productId={id} product={product} />;
 }
